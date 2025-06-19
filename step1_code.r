2e79c9105359ffb6a5bf209d05934e77f55707eb
Lianhao Song <hatsunecode@gmail.com>
2025-06-19 11:39:36 +0800

    Update step1_code.r

--- step1_code.r
+++ step1_code.r
@@ -1,31 +1,22 @@
-setwd("E:\\SYSU\\SLH\\GSE156928_RAW")
-file_list=read.table("file_list.txt")
+## tar -xvf GSE156928_RAW.tar
+## for i in *.tar.gz; do tar -xvf $i; done
 
-files <- list.files(path = "./", 
-                    pattern = "quant.genes.sf", 
-                    full.names = TRUE, 
-                    recursive = TRUE)
-matrix_A=NULL
-for(i in 1:length(files)){
-  data=read.table(files[i],sep="\t",header=T)
-  matrix_A=cbind(matrix_A,as.integer(data$NumReads))
-}
-colnames(matrix_A)=file_list$V1
-rownames(matrix_A)=data$Name
+## 1. read tables
+fs = list.files(pattern = 'quant.genes.sf', full.names = T, recursive = T)
+expr = do.call(cbind, lapply(fs, function(f) {
+  exp = read.table(f, sep = '\t', header = T)[, c('Name', 'NumReads')]
+  setNames(data.frame(exp$NumReads, row.names = exp$Name), unlist(strsplit(f, '/'))[2] )
+}))
+rownames(expr) = sub('\\..*', '', rownames(expr))
 
+## 2. id map
+ID  = read.table('IDtoSym.GRCh38-2024-A.txt', sep = '\t')
+Sym = ID$V2[match(rownames(expr), ID$V1)] 
+rownames(expr) = ifelse(is.na(Sym), rownames(expr), paste0(rownames(expr), '_', Sym))
+expr = RNAseq.checkDupRow(expr)
+expr = round(expr)
+write.table(cbind(Gene = rownames(expr), expr), 'Raw.counts.txt', sep = '\t', row.names = F)
 
-gene_I_S_list=read.delim("../IDtoSym.GRCh38-2024-A.txt",header=F,sep="\t")
-
-ENSG_ID=sub("\\.\\d+$", "",data$Name)
-loc=match(ENSG_ID,gene_I_S_list$V1)
-ENSG_ID1=ENSG_ID
-for(i in 1:length(ENSG_ID1)){
-  if(!is.na(loc[i])){
-  ENSG_ID1[i]=gene_I_S_list[loc[i],2]
-    }
-  
-}
-
-rownames(matrix_A)=ENSG_ID1
-normalize_exp=RNAseq.Normalize(matrix_A,log2=T,method = "DESeq2")
-
+## 3. normalize
+expr = RNAseq.Normalize(expr)
+write.table(cbind(Gene = rownames(expr), expr), 'Normalized.counts.txt', sep = '\t', row.names = F)
